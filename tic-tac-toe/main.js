07ab467434bbc6d35bc9be1355ded69cbe21b8f8
Justin Lee <joonsmoons@gmail.com>
2022-10-08 11:46:00 +0900

    [update] factory function for players, module for game  board

--- tic-tac-toe/main.js
+++ tic-tac-toe/main.js
@@ -14,4 +14,82 @@
 //   }
 // });
 
-const gameBoard = ["O", "", "", "O", "", "", "O", "", ""];
+// index wins
+// 012, 345, 678, 036, 147, 258, 048
+
+// object and methods to initialize and display gameBoard
+
+const game = (() => {
+  const gameBoard = ["", "", "", "", "", "", "", "", ""];
+  const positionResidue = [...Array(9).keys()];
+  const gameEnd = false;
+  const gameCells = document.querySelectorAll(".grid-item");
+  const displayController = () => {
+    gameBoard.map((item, index) => {
+      //   console.log(item, index, gameCells[index]);
+      const gameCell = gameCells[index];
+      if (gameCell.innerText === "") {
+        // prevent duplicate child write
+        const insertCell = document.createTextNode(item);
+        gameCell.appendChild(insertCell);
+      }
+    });
+  };
+
+  return {
+    gameBoard,
+    positionResidue,
+    gameEnd,
+    displayController,
+  };
+})();
+
+const Player = (tool, name = "player") => {
+  const playHistory = [];
+  const getName = () => name;
+  const getTool = () => tool; // O or X
+  const getPlayHistory = () => playHistory;
+
+  const playGame = (playPosition) => {
+    const positionResidue = game.positionResidue;
+    if (name == "computer") {
+      // choose random position from residue if computer
+      playPosition =
+        positionResidue[Math.floor(Math.random() * positionResidue.length)];
+    }
+    game.gameBoard[playPosition] = tool;
+    playHistory.push(playPosition);
+    game.positionResidue = positionResidue.filter((cell) => {
+      // update game position residue
+      return cell !== playPosition;
+    });
+    game.displayController();
+  };
+
+  if (name === "player") {
+    document.addEventListener("click", function (e) {
+      if (e.target.id) {
+        const gridElement = document.querySelector(`#${e.target.id}`);
+        const playPosition = e.target.id.replace(/^\D+/g, "");
+        playGame(+playPosition);
+        console.log(name, playPosition);
+        // const playPosition = grid
+      }
+    });
+  }
+
+  return { getTool, getPlayHistory, playGame };
+};
+
+// choose O or X
+
+// const Controller = (player, computer) => {
+//   const currentGameBoard = game.gameBoard;
+// };
+
+const player = Player("O", "player");
+const computer = Player("X", "computer");
+
+// player manually selects first cell
+// computer randomly selects first cell
+// player manually selects second cell
